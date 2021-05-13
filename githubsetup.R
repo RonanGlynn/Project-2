42644c4479e721d7a7fd514fbd50736e7db58191
Ronan Glynn <Ronan.Glynn@irishrail.ie>
2021-05-13 15:09:57 +0100

    e

--- githubsetup.R
+++ githubsetup.R
@@ -8,6 +8,10 @@ library(usethis)
 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
 
 
+edit_r_environ()
+
+# add: GITHUB_PAT="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"
+
 
 
 use_github(protocol="https", auth_token="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")
