7fa3bae4ae5d1cb60ccc142ae6770039fe520325
Ronan Glynn <Ronan.Glynn@irishrail.ie>
2021-05-13 15:08:40 +0100

    Edit 1

---
+++ githubsetup.R
@@ -0,0 +1,15 @@
+
+install.packages("usethis")
+
+library(usethis)
+
+# personal access token 
+
+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
+
+
+
+
+use_github(protocol="https", auth_token="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")
+
+Sys.getenv()
